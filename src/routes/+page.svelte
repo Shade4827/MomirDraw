490185611944b311f3f4043de43506a2d07200f2
Shade4827 <shade4827@gmail.com>
2025-08-19 20:05:43 +0900

    同じカードが出た場合は再抽選を促すように変更

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,6 +53,12 @@
       return;
     }
 
+    if (pastCards.some(card => card.id === displayCard.id)) {
+      errorMessage = '同じカードが既に抽選されています。再度お試しください。';
+      saving = false;
+      return;
+    }
+
     if (currentCard) {
       pastCards = [currentCard, ...pastCards];
     }
