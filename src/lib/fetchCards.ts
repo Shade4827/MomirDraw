d0296a148f939577f8ca81e2051e6504dcde598f
Shade4827 <shade4827@gmail.com>
2025-12-27 15:25:38 +0900

    URLのエンコード範囲を修正

--- src/lib/fetchCards.ts
+++ src/lib/fetchCards.ts
@@ -4,7 +4,7 @@ const API_URL = 'https://api.scryfall.com';
 
 export async function fetchRandomCardFromAPI(query: string): Promise<ScryfallCardResponse | null> {
   try {
-    const res = await fetch(`${API_URL}/cards/random?q=${query}`);
+    const res = await fetch(`${API_URL}/cards/random?q=${encodeURI(query)}`);
     if (!res.ok) throw new Error('API response not ok');
     return await res.json();
   } catch (error) {
